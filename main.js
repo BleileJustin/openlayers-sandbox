3cbf6c754405c1a1be6ea8440796c91392c15179
JustinBleile <justinbleile.dev@gmail.com>
2024-01-03 11:04:42 -0700

    Added capacity for user to choose imagery source

--- main.js
+++ main.js
@@ -55,6 +55,28 @@ const visualizations = [
   },
 ];
 
+const images = [
+  {
+    name: 'Buenos Aires',
+    base: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A',
+  },
+  {
+    name: 'Minneapolis',
+    base: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/15/T/WK/2021/9/S2B_15TWK_20210918_0_L2A',
+  },
+  {
+    name: 'Cape Town',
+    base: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/34/H/BH/2021/9/S2B_34HBH_20210922_0_L2A',
+  },
+];
+
+const imageSelector = document.getElementById('image');
+images.forEach((image) => {
+  const option = document.createElement('option');
+  option.textContent = image.name;
+  imageSelector.appendChild(option);
+});
+
 const visualizationSelector = document.getElementById('visualization');
 visualizations.forEach((visualization) => {
   const option = document.createElement('option');
@@ -80,16 +102,21 @@ const map = new Map({
   target: 'map-container',
 });
 
+let previousBase;
 function updateVisualization() {
   const visualization = visualizations[visualizationSelector.selectedIndex];
-  const base =
-    'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A';
+  const base = images[imageSelector.selectedIndex].base;
+  const newBase = base !== previousBase;
+  previousBase = base;
 
   const layer = createLayer(base, visualization);
   map.setLayers([layer]);
 
-  map.setView(layer.getSource().getView());
+  if (newBase) {
+    map.setView(layer.getSource().getView());
+  }
 }
 
 visualizationSelector.addEventListener('change', updateVisualization);
+imageSelector.addEventListener('change', updateVisualization);
 updateVisualization();
