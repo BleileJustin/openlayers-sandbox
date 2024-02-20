69564b6f03e2a107223803b874ca962729873ae0
JustinBleile <justinbleile.dev@gmail.com>
2024-02-20 09:33:37 -0700

    WebGL Point Rendering: Renders animation showing meteorite impacts over time

--- main.js
+++ main.js
@@ -1,31 +1,115 @@
-import MapboxVectorLayer from 'ol/layer/MapboxVector';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import {Map, View} from 'ol';
-import {Stroke, Style} from 'ol/style';
-import {fromLonLat} from 'ol/proj';
 import Feature from 'ol/Feature';
-import {fromExtent} from 'ol/geom/Polygon';
+import MapboxVectorLayer from 'ol/layer/MapboxVector';
+import Point from 'ol/geom/Point';
+import TileLayer from 'ol/layer/Tile';
+import WebGLPointsLayer from 'ol/layer/WebGLPoints';
+import {Map, View} from 'ol';
+import {Stamen, Vector as VectorSource} from 'ol/source';
+import {fromLonLat} from 'ol/proj';
+
+const source = new VectorSource();
+
+const client = new XMLHttpRequest();
+client.open('GET', './data/meteorites.csv');
+client.onload = function () {
+  const csv = client.responseText;
+  const features = [];
+
+  let prevIndex = csv.indexOf('\n') + 1; // scan past the header line
+
+  let curIndex;
+  while ((curIndex = csv.indexOf('\n', prevIndex)) != -1) {
+    const line = csv.substr(prevIndex, curIndex - prevIndex).split(',');
+    prevIndex = curIndex + 1;
+
+    const coords = fromLonLat([parseFloat(line[4]), parseFloat(line[3])]);
+    if (isNaN(coords[0]) || isNaN(coords[1])) {
+      // guard against bad data
+      continue;
+    }
+
+    features.push(
+      new Feature({
+        mass: parseFloat(line[1]) || 0,
+        year: parseInt(line[2]) || 0,
+        geometry: new Point(coords),
+      })
+    );
+  }
+  source.addFeatures(features);
+};
+client.send();
+const minYear = 1850;
+const maxYear = 2015;
+const span = maxYear - minYear;
+const rate = 10; // years per second
+
+const start = Date.now();
+
+const styleVariables = {
+  currentYear: minYear,
+};
+
+const period = 10;
+const periodStart = ['-', ['var', 'currentYear'], period];
+const decay = [
+  'interpolate',
+  ['linear'],
+  ['get', 'year'],
+  periodStart,
+  0,
+  ['var', 'currentYear'],
+  1,
+];
+
+const meteorites = new WebGLPointsLayer({
+  source: source,
+  style: {
+    symbol: {
+      symbolType: 'circle',
+      size: [
+        '*',
+        decay,
+        ['+', ['*', ['clamp', ['*', ['get', 'mass'], 1 / 20000], 0, 1], 18], 8],
+      ],
+      color: 'rgb(255, 0, 0)',
+      opacity: ['*', 0.5, decay],
+    },
+    variables: styleVariables,
+    filter: ['between', ['get', 'year'], periodStart, ['var', 'currentYear']],
+  },
+  disableHitDetection: true,
+});
 
 const map = new Map({
   target: 'map-container',
+  layers: [
+    new TileLayer({
+      source: new Stamen({
+        layer: 'toner',
+      }),
+    }),
+  ],
   view: new View({
-    center: fromLonLat([0, 0]),
+    center: [0, 0],
     zoom: 2,
   }),
 });
 
-const source = new VectorSource();
-new VectorLayer({
-  map: map,
-  source: source,
-  style: new Style({
-    stroke: new Stroke({
-      color: 'red',
-      width: 4,
-    }),
-  }),
-});
+const yearElement = document.getElementById('year');
+
+function render() {
+  const elapsed = (rate * (Date.now() - start)) / 1000;
+  styleVariables.currentYear = Math.round(minYear + (elapsed % span));
+  yearElement.innerText = styleVariables.currentYear;
+
+  map.render();
+  requestAnimationFrame(render);
+}
+
+render();
+
+// Import the necessary module
 
 const layer = new MapboxVectorLayer({
   styleUrl:
@@ -34,18 +118,6 @@ const layer = new MapboxVectorLayer({
   // styleUrl: 'mapbox://styles/mapbox/bright-v9',
   // accessToken: 'Your token from https://mapbox.com/'
 });
-map.addLayer(layer);
 
-map.on('pointermove', function (event) {
-  source.clear();
-  map.forEachFeatureAtPixel(
-    event.pixel,
-    function (feature) {
-      const geometry = feature.getGeometry();
-      source.addFeature(new Feature(fromExtent(geometry.getExtent())));
-    },
-    {
-      hitTolerance: 2,
-    }
-  );
-});
+map.addLayer(layer);
+map.addLayer(meteorites);
